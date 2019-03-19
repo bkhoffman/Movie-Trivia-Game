a028ddb4cbe96a682534f2daf0b09e285d0b2530
boffman <bkhoffman@gmail.com>
2019-03-19 17:22:41 -0500

    added objects with correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,36 @@
-var questions = ["question1", "questions2", "question3", "question4", "questions5", "question6", "question7", "question8"];
-var answer1 = ["a", "b", "c"]
-var answer2 = ["d", "e", "f"]
-var answer3 = ["g", "h", "i"]
+//var questions = ["question1", "questions2", "question3", "question4", "questions5", "question6", "question7", "question8"];
+var questionsAsked = []; //push questions after asked
+// var answer1 = ["a", "b", "c"]
+// var answer2 = ["d", "e", "f"]
+// var answer3 = ["g", "h", "i"]
 
 var questionTimer = 31;
 //var timerRunning = false;
+
+//create an object holding the questions with the answers?
+var question = [
+  {
+    ask: "question1",
+    answer1 : "a",
+    answer2 : "b",
+    answer3 : "c",
+    correct : "2"
+  },
+  {
+    ask: "question2",
+    answer1 : "d",
+    answer2 : "e",
+    answer3 : "f",
+    correct : "3"
+  },
+  {
+    ask: "question3",
+    answer1 : "g",
+    answer2 : "h",
+    answer3 : "i",
+    correct : "1"
+  }
+]
 
 $("#answer1").prop("hidden", true);
 $("#answer2").prop("hidden", true);
@@ -16,13 +42,21 @@ $("#startButton").on("click", function startGame(){
   $(this).prop("hidden",true);
   $("#timer-text").html("Time Remaining: ");
   intervalId = setInterval(count, 1000);
+  //pick random question from questions array and display
+  //var startQuestion = questions[Math.floor(Math.random() * questions.length)];
+  //$("#questions").text(startQuestion);
 
+  $("#answer1").prop("hidden", false);
+  $("#answer2").prop("hidden", false);
+  $("#answer3").prop("hidden", false);
 
-
-  questions.forEach(function(){
-    $("#answer1").html(answer1[i]);
-    $("#answer2").html(answer2[i]);
-    $("#answer3").html(answer3[i]);
+  question.forEach(function(){
+    var startQuestion = question[Math.floor(Math.random() * question.length)];
+    console.log("picked question", startQuestion);
+    $("#questions").text(startQuestion.ask);
+    $("#answer1").html(startQuestion.answer1);
+    $("#answer2").html(startQuestion.answer2);
+    $("#answer3").html(startQuestion.answer3);
   });
 });
 
@@ -32,5 +66,15 @@ function count() {
   // Display the timer
   $("#timeRemaining").text(questionTimer);
 }
+//var answers = $("#answer1", "#answer2", "#answer3");
+$("#answer1").click(function answerCheck(){
+  console.log("answered!")
+  //if (question[0] && answer2 || question[1] && answer1 || question[2] && answer3 ){
+      //console.log("You Win") //flash picture
+  //} else {
+      //console.log("Incorrect") // show correct answer and picture
+  // };
+});
+
 
 
