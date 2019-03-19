cbbe87270300634fe0fdb66cf078e2475de376ab
boffman <bkhoffman@gmail.com>
2019-03-19 15:12:35 -0500

    hide the answer options, hide the start button after pushed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,26 @@ var answer1 = ["a", "b", "c"]
 var answer2 = ["d", "e", "f"]
 var answer3 = ["g", "h", "i"]
 
-var questionTimer = 30;
+var questionTimer = 31;
 //var timerRunning = false;
 
+$("#answer1").prop("hidden", true);
+$("#answer2").prop("hidden", true);
+$("#answer3").prop("hidden", true);
 
-$(".btn").on("click", function startGame(){
+$("#startButton").on("click", function startGame(){
   console.log("click");
+  //hide the start button, start the timer, show the questions and answers
+  $(this).prop("hidden",true);
   $("#timer-text").html("Time Remaining: ");
   intervalId = setInterval(count, 1000);
 
+
+
   questions.forEach(function(){
-    $("#answer1").html()
+    $("#answer1").html(answer1[i]);
+    $("#answer2").html(answer2[i]);
+    $("#answer3").html(answer3[i]);
   });
 });
 
